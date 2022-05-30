060fa7cba3e7be99c9b6cc1f65632a232ece06d7
SurgeCode <sergioseco98@gmail.com>
2022-05-30 15:41:52 -0700

    Get Top Rated

--- movie-smt-bff/controller/movies_controller.go
+++ movie-smt-bff/controller/movies_controller.go
@@ -9,7 +9,8 @@ import (
 
 func Routes(router *gin.Engine) {
 	router.GET("/search", search)
-	router.GET("/popular", getPopular)
+	router.GET("/movie/popular", getPopular)
+	router.GET("/movie/top_rated", getTopRated)
 }
 
 func search(c *gin.Context) {
@@ -34,6 +35,20 @@ func getPopular(c *gin.Context) {
 	}
 
 	res, err := service.GetPopularMovies(page)
+	if err == nil {
+		c.JSON(http.StatusOK, res)
+	} else {
+		c.JSON(http.StatusInternalServerError, internalServerErrorError)
+	}
+
+}
+
+func getTopRated(c *gin.Context) {
+	page := c.Query("page")
+	if page == "" {
+		page = "1"
+	}
+	res, err := service.GetTopRatedMovies(page)
 	if err == nil {
 		c.JSON(http.StatusOK, res)
 	} else {
